606de2c9766fcb594ec8adc8fc649f7b2261e0c2
Mohamed Emary <mohamed.emary@ecomexperts.io>
2025-02-06 17:42:13 +0200

    [BUILD]: Include all liquid files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./layout/*.liquid", "./sections/*.liquid"],
+  content: ["./**/*.liquid"],
   theme: {
     extend: {},
   },
